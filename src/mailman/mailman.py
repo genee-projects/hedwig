a625540b5e7c8e4943274c2669175c02600bb3be
Rui Ma <rui.ma@geneegroup.com>
2016-02-26 16:50:11 +0800

    开发完成 mailman

--- src/mailman/mailman.py
+++ src/mailman/mailman.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import asyncore, smtpd
+import asyncore, smtpd, requests, yaml
 
 
 class GServer(smtpd.SMTPServer):
+    '''
+    继承自smtpd.SMTPServer
+    用于把邮件发送请求按照 POST 请求发送到 robots.smtp.genee.cn
+    '''
 
+    # 收到邮件发送请求后, 进行邮件发送
     def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
-        pass
+
+        config = yaml.load('config.yml')
+
+        data = {
+            'key': config['key'],
+            'fqdn': config['fqdn'],
+            'mailfrom': mailfrom,
+            'rcpttos': rcpttos,
+            'data': data
+        }
+
+        requests.post(
+            config.get('url', 'robots.smtp.genee.cn'),
+            data=data,
+            timeout=config.get('timeout', 5)
+        )
 
 
 def main():
