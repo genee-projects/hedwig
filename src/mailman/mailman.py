d85b221424b3bcb8a9484fc2738b1378407afe9f
Rui Ma <rui.ma@geneegroup.com>
2016-02-29 13:12:09 +0800

    mailman.py 开发完成

--- src/mailman/mailman.py
+++ src/mailman/mailman.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import asyncore, smtpd, requests, yaml
+import asyncore
+import smtpd
+import requests
+import yaml
+import json
+import sys
+import logging
+
+logger = logging.getLogger('Mailman')
 
 
-class GServer(smtpd.SMTPServer):
-    '''
+class Mailman(smtpd.SMTPServer):
+    """
     继承自smtpd.SMTPServer
     用于把邮件发送请求按照 POST 请求发送到 robots.smtp.genee.cn
-    '''
+    """
+
+    def __init__(*args, **kwargs):
+
+        logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
+
+        if 'debug' in kwargs:
+            logger.setLevel(logging.DEBUG)
+            del kwargs['debug']
+            logger.debug('Running Debug Mode')
+        else:
+            logger.setLevel(logging.INFO)
+
+        logger.info('Running Mailman on port 25')
+
+        smtpd.SMTPServer.__init__(*args, **kwargs)
 
     # 收到邮件发送请求后, 进行邮件发送
     def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
 
-        config = yaml.load('config.yml')
+        with open('config.yml') as f:
+            config = yaml.load(f)
 
         data = {
             'key': config['key'],
@@ -23,21 +47,31 @@ class GServer(smtpd.SMTPServer):
             'data': data
         }
 
+        logger.debug('mailfrom: {mailfrom}, rcpttos: {rcpttos}, data: {data}'.format(
+            mailfrom=mailfrom,
+            rcpttos=json.dumps(rcpttos),
+            data=json.dumps(data)
+        ))
+
         requests.post(
-            config.get('url', 'robots.smtp.genee.cn'),
-            data=data,
+            config.get('url', 'http://robots.smtp.genee.cn/'),
+            data=json.dumps(data),
             timeout=config.get('timeout', 5)
         )
+
+        return None
 
 
 def main():
 
-    _ = GServer(('0.0.0.0', 25), None)
+    debug = True if '--deubg' in sys.argv or '-d' in sys.argv else False
+
+    mm = Mailman(('0.0.0.0', 25), None, debug=debug)
 
     try:
         asyncore.loop()
     except KeyboardInterrupt:
-        pass
+        mm.close()
 
 if __name__ == '__main__':
     main()
